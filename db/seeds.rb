5cbc3ad3af91f939d2ab385256164d37b9340b7c
Alice <alicewc95@gmail.com>
2017-12-15 22:05:32 -0800

    checkpoint-27

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'random_data'
 
+# Create Users
+ 5.times do
+   User.create!(
+ # #3
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+
 15.times do
      Topic.create!(
           name: RandomData.random_sentence,
@@ -18,7 +29,8 @@ topics = Topic.all
 50.times do
 
     Post.create!(
-         topic: topics.sample,
+        user:   users.sample,
+        topic: topics.sample,
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph
     )
@@ -57,7 +69,13 @@ question = Question.all
     )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'alicewc95@gmail.com', # replace this with your personal email
+  password: 'helloworld'
+)
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} post created"
 puts "#{Comment.count} comments created"
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,10 +1,75 @@
 require 'rails_helper'
 
+include SessionsHelper
+
 RSpec.describe PostsController, type: :controller do
+    let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+    let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
+    let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
 
-     let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
+    # #5
+       context "guest user" do
+     # #6
+         describe "GET show" do
+           it "returns http success" do
+             get :show, params: {topic_id: my_topic.id, id: my_post.id}
+             expect(response).to have_http_status(:success)
+           end
 
-     let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
+           it "renders the #show view" do
+             get :show, params: {topic_id: my_topic.id, id: my_post.id}
+             expect(response).to render_template :show
+           end
+
+           it "assigns my_post to @post" do
+             get :show, params: {topic_id: my_topic.id, id: my_post.id}
+             expect(assigns(:post)).to eq(my_post)
+           end
+         end
+
+     # #7
+         describe "GET new" do
+           it "returns http redirect" do
+             get :new, params: {topic_id: my_topic.id}
+     # #8
+             expect(response).to redirect_to(new_session_path)
+           end
+         end
+
+         describe "POST create" do
+           it "returns http redirect" do
+             post :create, params: {topic_id: my_topic.id, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
+             expect(response).to redirect_to(new_session_path)
+           end
+         end
+
+         describe "GET edit" do
+           it "returns http redirect" do
+             get :edit, params: {topic_id: my_topic.id, id: my_post.id}
+             expect(response).to redirect_to(new_session_path)
+           end
+         end
+
+         describe "PUT update" do
+           it "returns http redirect" do
+             new_title = RandomData.random_sentence
+             new_body = RandomData.random_paragraph
+
+             put :update, params: {topic_id: my_topic.id, id: my_post.id, post: {title: new_title, body: new_body}}
+             expect(response).to redirect_to(new_session_path)
+           end
+         end
+
+         describe "DELETE destroy" do
+           it "returns http redirect" do
+             delete :destroy, params: {topic_id: my_topic.id, id: my_post.id}
+             expect(response).to have_http_status(:redirect)
+           end
+         end
+       end
+       context "signed-in user" do
+        before do
+          create_session(my_user)
 
 =begin
     describe "GET index" do
@@ -125,5 +190,6 @@ RSpec.describe PostsController, type: :controller do
             expect(response).to redirect_to my_topic
         end
     end
-
+  end
+end
 end
