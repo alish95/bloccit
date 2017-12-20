838d5207f485f32d6a24de3d351ec73e0e00ed4b
Alice <alicewc95@gmail.com>
2017-12-19 16:00:39 -0800

    checkpoint-30

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,98 +7,70 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'random_data'
 
-# Create Users
- 5.times do
-   User.create!(
- # #3
-   name:     RandomData.random_name,
-   email:    RandomData.random_email,
-   password: RandomData.random_sentence
-   )
- end
- users = User.all
+5.times do
+  User.create!(
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
 
 15.times do
-     Topic.create!(
-          name: RandomData.random_sentence,
-          description: RandomData.random_paragraph
-     )
+  Topic.create!(
+    name:        RandomData.random_sentence,
+    description: RandomData.random_paragraph
+  )
 end
 topics = Topic.all
 
 50.times do
+  post = Post.create!(
+    user:   users.sample,
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
 
-    Post.create!(
-        user:   users.sample,
-        topic: topics.sample,
-        title: RandomData.random_sentence,
-        body: RandomData.random_paragraph
-    )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 
-    SponsoredPost.create!(
-         topic: topics.sample,
-         title: RandomData.random_sentence,
-         body: RandomData.random_paragraph,
-         price: rand(50)
-    )
-
-  ##  Advertisement.create!(
-        ##title: RandomData.random_sentence,
-        ##body: RandomData.random_paragraph,
-        ##price: rand(50)
-   ##)
-
-    Question.create!(
-        title: RandomData.random_sentence,
-        body: RandomData.random_paragraph,
-        resolved: [true, false].sample
-    )
 end
-post = Post.all
-question = Question.all
+posts = Post.all
 
 100.times do
-    Comment.create!(
-        user: users.sample,
-        post: post.sample,
-        body: RandomData.random_paragraph
-    )
-
-    Answer.create!(
-         question: question.sample,
-         body: RandomData.random_paragraph
-    )
+  Comment.create!(
+    user: users.sample,
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
 end
 
-# Create an admin user
- admin = User.create!(
-   name:     'Admin User',
-   email:    'admin@example.com',
-   password: 'helloworld',
-   role:     'admin'
- )
+100.times do
+  Question.create!(
+    title:    RandomData.random_sentence,
+    body:     RandomData.random_paragraph,
+    resolved: false
+  )
+end
 
- # Create a member
- member = User.create!(
-   name:     'Member User',
-   email:    'member@example.com',
-   password: 'helloworld'
- )
-
- moderator = User.create!(
-  name:     'Moderator User',
-  email:    'moderator@example.com',
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
   password: 'helloworld',
-  role:     'moderator'
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
 )
 
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
-puts "#{Post.count} post created"
+puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
-
-##puts "#{Advertisement.count} ads created"
-puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Question.count} questions created"
-puts "#{Answer.count} answers created"
+puts "#{Vote.count} votes created"
